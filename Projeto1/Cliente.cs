50411b9fdd2b40cea92903d66ba13a0d3bbac81e
NANDO9322 <andre.fr93@gmail.com>
2023-10-09 12:29:05 -0300

    Aula 09.10.23

---
+++ Projeto1/Cliente.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+
+
+namespace Projeto1
+{
+    public sealed class Cliente    
+    {      
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Endereco { get; private set;}
+
+        public Cliente(int id, string nome, string endereco)
+        {
+            EstruturaCliente(id, nome, endereco);
+            Id = id;
+            Nome = nome;
+            Endereco = endereco;
+        }
+        private void EstruturaCliente(int id, string nome, string endereco)
+        {
+            if (id < 0)
+            {
+                throw new InvalidOperationException("O id esta invalido");
+            }
+            if (string.IsNullOrEmpty(nome) || string.IsNullOrEmpty(endereco)) 
+            {
+                throw new InvalidOperationException("O endereço ou nome nao podem estar vazios");
+            }
+        }
+
+    }
+
+   
+}
---
+++ Projeto2/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto2
+{
+    public sealed class Produto
+    {
+        public int Id { get; private set; }
+        public string Descricao { get; private set; }
+        public string Fabricante { get; private set; }
+        public int Valor { get; private set; }
+
+        public Produto(int id, string descricao, string fabricante, int valor)
+        {
+            EstruturaProduto( id, valor);
+            Id = id;
+            Descricao = descricao;
+            Fabricante = fabricante;
+            Valor = valor;
+        }
+        private void EstruturaProduto(int id, int valor)
+        {
+            if(id<0)
+            {
+                throw new InvalidOperationException("O id esta invalido");
+            }
+            if (valor<0)
+            {
+                throw new InvalidOperationException("Não é possivel registrar valores negativos");
+            }
+        } 
+    }
+}
